c163ea062c4a60604de1817bc0995f55605f6903
aprigantengkali06-collab <aprigantengkali06@gmail.com>
2026-03-21 07:10:17 +0000

    fix: next.config.mjs pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@ import withPWA from "next-pwa";
 
 const nextConfig = withPWA({
   dest: "public",
-    register: true,
-      skipWaiting: true,
-        disable: process.env.NODE_ENV === "development",
-        });
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+})({});
 
-        export default nextConfig;
+export default nextConfig;
