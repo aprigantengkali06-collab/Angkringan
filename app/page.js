bf630ed1963eb659e78b2f7c96c807f5c3b99174
aprigantengkali06-collab <aprigantengkali06@gmail.com>
2026-03-29 02:31:29 +0700

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
-"use client";
-import { useState, useEffect } from "react";
 import AngkringanApp from "../components/AngkringanApp";
 
 export default function Page() {
-  const [ready, setReady] = useState(false);
-  useEffect(() => { setReady(true); }, []);
-  if (!ready) return null;
   return <AngkringanApp />;
-}
+    }
